95a8987443247a0136edc04be16d8871f958af9d
julitofm93 <julian.fm@live.com>
2023-02-01 18:02:47 -0300

    final modifications

--- client/src/pages/Books.jsx
+++ client/src/pages/Books.jsx
@@ -1,8 +1,59 @@
 import React from 'react'
+import { useEffect } from 'react'
+import { useState } from 'react'
+import axios from "axios";
+import { Link } from "react-router-dom";
 
 const Books = () => {
+
+  const [books, setBooks] = useState([])
+
+  useEffect(()=>{
+    const fetchAllBooks = async ()=>{
+      try {
+        const res = await axios.get("http://localhost:8800/books")
+        setBooks(res.data);
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    fetchAllBooks()
+  })
+
+  const handleDelete = async (id) =>{
+    try {
+      await axios.delete("http://localhost:8800/books/"+id)
+      window.location.reload()
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   return (
-    <div>Books</div>
+    <div>
+      <h1>Book Shop</h1>
+      <div className="books">
+        {books.map(book=>(
+          <div className="book" key={book.id}>
+            {book.cover && <img src={book.cover} alt="" />}
+            <h2>{book.title}</h2>
+            <p>{book.desc}</p>
+            <span>{book.price}</span>
+            <button className='delete' onClick={()=>handleDelete(book.id)}>Borrar</button>
+            <button className='update'>
+              <Link to={`/update/${book.id}`}>
+                Editar
+              </Link>
+            </button>
+          </div>
+        ))}
+      </div>
+      <button>
+        <Link to="/add">
+          Agregar nuevo
+        </Link>
+      </button>
+    </div>
   )
 }
 
--- client/src/pages/Update.jsx
+++ client/src/pages/Update.jsx
@@ -1,8 +1,47 @@
 import React from 'react'
+import { useState } from 'react'
+import axios from "axios"
+import {useLocation, useNavigate} from "react-router-dom"
 
 const Update = () => {
+  const [book,setBook] = useState({
+    title:"",
+    desc:"",
+    price: null,
+    cover:"",
+  });
+
+  const navigate = useNavigate()
+  const location = useLocation()
+
+  const bookId = location.pathname.split("/")[2]
+
+  console.log(bookId)
+  const handleChange = (e) =>{
+    setBook(prev=>({...prev, [e.target.name]: e.target.value }));
+  };
+
+  const handleClick = async e =>{
+    e.preventDefault()
+    try {
+      await axios.put(`http://localhost:8800/books/${bookId}`, book)
+      navigate("/")
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  console.log(book)
+
   return (
-    <div>Update</div>
+    <div className='form'>
+      <h1>Editar nuevo</h1>
+      <input type="text" placeholder='title' onChange={handleChange} name='title'/>
+      <input type="text" placeholder='desc'onChange={handleChange} name='desc'/>
+      <input type="number" placeholder='price' onChange={handleChange} name='price'/>
+      <input type="text" placeholder='cover' onChange={handleChange} name='cover'/>
+      <button className='formButton' onClick={handleClick}>Agregar</button>
+    </div>
   )
 }
 
