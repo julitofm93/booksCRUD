89243f58f559fcf7675cd7b7423ca947db2f356d
julitofm93 <julian.fm@live.com>
2023-02-01 13:12:33 -0300

    first commit

---
+++ backend/index.js
@@ -0,0 +1,45 @@
+import express from "express";
+import mysql from "mysql";
+
+const app = express()
+
+const db = mysql.createConnection({
+    host:"localhost",
+    user:'root',
+    password:"admin",
+    database:"test"
+})
+
+//ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'admin'
+
+app.use(express.json())
+
+app.get("/", (req,res)=>{
+    res.json("hello this is the backend")
+})
+
+app.get("/books", (req,res)=>{
+    const q = "SELECT * FROM books"
+    db.query(q,(err,data)=>{
+        if(err) return res.json(err)
+        return res.json(data)
+    })
+})
+
+app.post("/books",(req,res)=>{
+    const q = "INSERT INTO books (`title`, `desc`, `cover`) VALUES (?)"
+    const values = [
+        req.body.title,
+        req.body.desc,
+        req.body.cover
+    ]
+
+    db.query(q,[values], (err,data)=>{
+        if(err) return res.json(err)
+        return res.json("Book has been created")
+    })
+})
+
+app.listen(8800, ()=>{
+    console.log("backend connected")
+})
